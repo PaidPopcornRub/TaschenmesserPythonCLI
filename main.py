8589a0dbce1458dfd5b98c0b8c4303666d364cda
PaidPopcornRub <spamrubennichtvoll@outlook.com>
2024-09-10 17:29:09 +0000

    Broke Code

--- main.py
+++ main.py
@@ -3,8 +3,48 @@ import random
 def Intro():
   print("--- Taschenrechner ---")
   print("Willkommen zum Taschenrechner, bitte gebe an was du machen willst.")
-  print("\n1. Zufallszahl\n Taschenrechner\n3. Irgendwas")
-  tool=input("Worauf hast du lust?")
+  print("\n1. Zufallszahl\n2. Taschenrechner\n3. Irgendwas\n4. Programm stoppen")
+
+  toolSelect()
+
+def toolSelect():
+  tool=input("Worauf hast du lust? ")
+  
+  if tool == 1:
+    Zufallszahl()
+  elif tool == 2:
+    Taschenrechner()
+  elif tool == 3:
+    Irgendwas()
+  elif tool == 4:
+    ProgrammStop()
+
+  else:
+    print("---")
+    print("Bitte gebe eine Valide eingabe ab!")
+    toolSelect()
+
+
+
+
+
+
+def Zufallszahl():
+  print("true")
+
+def Taschenrechner():
+  print("true")
+
+def Irgendwas():
+  print("true")
+
+def ProgrammStop():
+  exit()
+
+def ui():
+  print("true")
+
+def end():
+  print("true")
 
 Intro()
-
