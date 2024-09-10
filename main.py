482ea17ff34dd9efbf67b41e256ce6c0e5b10130
PaidPopcornRub <spamrubennichtvoll@outlook.com>
2024-09-10 16:09:25 +0000

    Added Intro def

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+def Intro():
+  print("--- Taschenrechner ---")
+  print("Willkommen zum Taschenrechner, bitte gebe an was du machen willst.")
+  print("\n1. Zufallszahl\n Taschenrechner\n3. Irgendwas")
+  tool=input("Worauf hast du lust?")
+
+Intro()
+
